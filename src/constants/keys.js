65f3dd3f197b676a595aa8865748f9f7eac25f81
JhuneAlfred <avelicaria@me.com>
2018-04-05 22:30:07 -0700

    .

---
+++ src/constants/keys.js
@@ -0,0 +1,2 @@
+
+export const API_KEY = 'AIzaSyCJ2bpTAELRvR4-pYVnS-S7YYPrLqeO5fo'
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 
-const API_KEY = 'AIzaSyCJ2bpTAELRvR4-pYVnS-S7YYPrLqeO5fo'
+import SearchBar from './components/search_bar'
+import API_KEY from './constants/keys'
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
     console.log(data)
