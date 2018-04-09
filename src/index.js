591eb7d6e62ba7bc1217b3e8ada2a90c4b139f84
JhuneAlfred <avelicaria@me.com>
2018-04-08 23:45:56 -0700

    .

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import api_key from './constants/keys'
-const API_KEY = 'AIzaSyCJ2bpTAELRvR4-pYVnS-S7YYPrLqeO5fo'
 
 //create a new component. this component should produce some html
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = { videos: [] }
-        console.log(API_KEY)
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+        console.log(api_key)
+        YTSearch({ key: api_key, term: 'surfboards' }, (videos) => {
             console.log(videos)
             this.setState({ videos })
         })
